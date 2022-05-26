66c24a6f5cdd57c0f71fa3f13cd48fc4f8f9f89b
Strelkova Olga <strelod@yandex.ru>
2022-05-26 17:10:18 +0300

    create component TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
 <h1>Todo List</h1>
 <hr>
+<TodoList />
 </template>
 
 <script>
-
+import TodoList from "@/components/TodoList"
 export default {
   name: 'App',
   components: {
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <ul>
+            <li>1. доделать проект 11</li>
+        </ul>
+    </div>
+</template>
