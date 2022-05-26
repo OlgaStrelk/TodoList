7c58754ef425a5c3c52ee4d8ee9a12ff2a04ca10
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-05-27 00:39:07 +0300

    add some styles

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,11 @@
 <template>
-    <li>
-        <span><input type="checkbox"><strong>{{ todo.id }}</strong>{{ todo.title }}</span><button>&times;</button>
+    <li class="task">
+        <span v-bind:class="{done: todo.completed}">
+            <input class="input" type="checkbox">
+            <strong class="count">{{ todo.id }}</strong>
+            {{ todo.title }}
+        </span>
+        <button class="remove-button">&times;</button>
     </li>
 </template>
 
@@ -16,5 +21,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.task {
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 20px;
+    margin-bottom: 10px;
+}
+
+.remove-button {
+    font-size: 30px;
+    padding: 0;
+    top: 8px;
+    right: 4px;
+    border: none;
+    cursor: pointer;
+    transition: opacity 0.5s linear;
+    opacity: 1;
+}
+
+.remove-button:hover {
+    opacity: 0.6;
+}
+
+input {
+    margin-right: 10px;
+}
+
+.done {
+    text-decoration: line-through;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-        <ul>
-            <TodoItem 
-            v-for="item of todos"
-            v-bind:todo="item"/>
+        <ul class="list">
+            <TodoItem v-for="item of todos" v-bind:todo="item" />
         </ul>
     </div>
 </template>
@@ -16,3 +14,13 @@ export default {
         TodoItem
     }
 }</script>
+
+<style scoped>
+.list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    max-width: 400px;
+    align-self: center;
+}
+</style>
